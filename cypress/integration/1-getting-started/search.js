cb15e83df291e412604d1b0e19ea43bcb59974e2
julio aleman <javierarb7530@gmail.com>
2022-05-31 13:41:46 -0600

    commands updated

--- cypress/integration/1-getting-started/search.js
+++ cypress/integration/1-getting-started/search.js
@@ -5,19 +5,14 @@ describe("Search elements", ()=>{
     })
     it("search for elements with multiple results",()=>{
 
-        cy.fixture('index').then((index)=>{
-            cy.get(index.searchBox).type('dress');
-            cy.get(index.searchButton).click();
-        })
+        cy.search('dress')
         cy.fixture('searchResult').then((searchResult)=>{
             cy.get(searchResult.title).should('contain','dress');
         })
     })
    it('search for elements with no results', ()=>{
-    cy.fixture('index').then((index)=>{
-        cy.get(index.searchBox).type('qwerty');
-        cy.get(index.searchButton).click();
-        }) 
+
+        cy.search('qwerty')
         cy.fixture('searchResult').then((searchResult)=>{
             cy.get(searchResult.alert).should('contain','No results were found for your search');
         })
